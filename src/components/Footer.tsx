22cd0097b15b30ec578c97540364c716e993507e
Deva Mahayana <devamahayanatop@gmail.com>
2025-04-17 18:01:44 +0800

    Update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,70 +1,59 @@
 import React from 'react'
 import Image from 'next/image'
-import Logo from '../../public/image/logo-potrait.svg'
+import Logo from '../../public/image/logo.png'
 import Link from 'next/link'
 
 const Footer = () => {
 
   return (
     <>
-    <section className='bg-cover bg-center bg-no-repeat'
-      style={{
-        background: `url(./image/villa/villa1.webp)`,
-        backgroundSize: `cover`,
-        backgroundPosition: `center`,
-        backgroundRepeat: `no-repeat`
-    }}>
-      <div className='bg-secondary/70'>
+
+      <div className='bg-primary'>
         <div className='py-16 2xl:w-4/5 w-11/12 m-auto flex flex-col gap-10'>
           <Image src={Logo} alt='Balimmo Logo' width={200} height={200} className='m-auto'/>
-
-          <div className='flex md:flex-row flex-col text-center items-center justify-center gap-3 text-white md:divide-x-2'>
-            <div className='flex items-center gap-2 fill-primary'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className='min-w-[24px]'><path d="M12 2C7.589 2 4 5.589 4 9.995 3.971 16.44 11.696 21.784 12 22c0 0 8.029-5.56 8-12 0-4.411-3.589-8-8-8zm0 12c-2.21 0-4-1.79-4-4s1.79-4 4-4 4 1.79 4 4-1.79 4-4 4z"></path></svg>
-              <h3 className=''>PT BALIMMO DEVELOPMENT GROUP, Jalan Pantai Batu Bolong No.15, Canggu, 80351 BALI</h3>
-            </div>            
-
-            <div className='flex items-center gap-2 pl-2 fill-primary'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24"><path d="m20.487 17.14-4.065-3.696a1.001 1.001 0 0 0-1.391.043l-2.393 2.461c-.576-.11-1.734-.471-2.926-1.66-1.192-1.193-1.553-2.354-1.66-2.926l2.459-2.394a1 1 0 0 0 .043-1.391L6.859 3.513a1 1 0 0 0-1.391-.087l-2.17 1.861a1 1 0 0 0-.29.649c-.015.25-.301 6.172 4.291 10.766C11.305 20.707 16.323 21 17.705 21c.202 0 .326-.006.359-.008a.992.992 0 0 0 .648-.291l1.86-2.171a.997.997 0 0 0-.085-1.39z"></path></svg>
-              <h3 className=''>+62 85 333 777 500</h3>
+          <div className='text-white flex justify-between'>
+            <div>
+              <h3 className='text-2xl mb-4'>Social media</h3>
+              <div className='flex flex-col justify-center gap-3 fill-white'>             
+              <Link href="https://www.instagram.com/oceane.dagostino" aria-label='socialmedia' target='_blank' className='flex items-center gap-2'>
+                <div className='fill-white flex items-center justify-center text-white bg-white/20 text-center w-[40px] h-[40px] rounded-full hover:bg-secondary hover:-translate-y-2 duration-500 transition-all'>          
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"></path><circle cx="16.806" cy="7.207" r="1.078"></circle><path d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z"></path></svg>
+                </div>
+                Instagram
+              </Link>
+              <Link href="https://www.linkedin.com/in/oc%C3%A9ane-d%E2%80%99agostino-b973662a1" aria-label='socialmedia' target='_blank' className='flex items-center gap-2'>
+                <div className='fill-white flex items-center justify-center text-white bg-white/20 text-center w-[40px] h-[40px] rounded-full hover:bg-secondary hover:-translate-y-2 duration-500 transition-all'>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><circle cx="4.983" cy="5.009" r="2.188"></circle><path d="M9.237 8.855v12.139h3.769v-6.003c0-1.584.298-3.118 2.262-3.118 1.937 0 1.961 1.811 1.961 3.218v5.904H21v-6.657c0-3.27-.704-5.783-4.526-5.783-1.835 0-3.065 1.007-3.568 1.96h-.051v-1.66H9.237zm-6.142 0H6.87v12.139H3.095z"></path></svg>
+                </div>
+                Linkedin
+              </Link>
+              <Link href="https://wa.me/33646761667" aria-label='socialmedia' target='_blank' className='flex items-center gap-2'>
+                <div className='fill-white flex items-center justify-center text-white bg-white/20 text-center w-[40px] h-[40px] rounded-full hover:bg-secondary hover:-translate-y-2 duration-500 transition-all'>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path fill-rule="evenodd" clip-rule="evenodd" d="M18.403 5.633A8.919 8.919 0 0 0 12.053 3c-4.948 0-8.976 4.027-8.978 8.977 0 1.582.413 3.126 1.198 4.488L3 21.116l4.759-1.249a8.981 8.981 0 0 0 4.29 1.093h.004c4.947 0 8.975-4.027 8.977-8.977a8.926 8.926 0 0 0-2.627-6.35m-6.35 13.812h-.003a7.446 7.446 0 0 1-3.798-1.041l-.272-.162-2.824.741.753-2.753-.177-.282a7.448 7.448 0 0 1-1.141-3.971c.002-4.114 3.349-7.461 7.465-7.461a7.413 7.413 0 0 1 5.275 2.188 7.42 7.42 0 0 1 2.183 5.279c-.002 4.114-3.349 7.462-7.461 7.462m4.093-5.589c-.225-.113-1.327-.655-1.533-.73-.205-.075-.354-.112-.504.112s-.58.729-.711.879-.262.168-.486.056-.947-.349-1.804-1.113c-.667-.595-1.117-1.329-1.248-1.554s-.014-.346.099-.458c.101-.1.224-.262.336-.393.112-.131.149-.224.224-.374s.038-.281-.019-.393c-.056-.113-.505-1.217-.692-1.666-.181-.435-.366-.377-.504-.383a9.65 9.65 0 0 0-.429-.008.826.826 0 0 0-.599.28c-.206.225-.785.767-.785 1.871s.804 2.171.916 2.321c.112.15 1.582 2.415 3.832 3.387.536.231.954.369 1.279.473.537.171 1.026.146 1.413.089.431-.064 1.327-.542 1.514-1.066.187-.524.187-.973.131-1.067-.056-.094-.207-.151-.43-.263"></path></svg>
+                </div>
+                Whatsapp
+              </Link>
+              <Link href="https://t.me/CEO_Invest_consultancy_Dubai" aria-label='socialmedia' target='_blank' className='flex items-center gap-2'>
+                <div className='fill-white flex items-center justify-center text-white bg-white/20 text-center w-[40px] h-[40px] rounded-full hover:bg-secondary hover:-translate-y-2 duration-500 transition-all'>
+                  <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="m20.665 3.717-17.73 6.837c-1.21.486-1.203 1.161-.222 1.462l4.552 1.42 10.532-6.645c.498-.303.953-.14.579.192l-8.533 7.701h-.002l.002.001-.314 4.692c.46 0 .663-.211.921-.46l2.211-2.15 4.599 3.397c.848.467 1.457.227 1.668-.785l3.019-14.228c.309-1.239-.473-1.8-1.282-1.434z"></path></svg>
+                </div>
+                Telegram
+              </Link>
+             
+            </div> 
+            </div>
+            <div>
+              <h3 className='text-2xl mb-4'>Gallery</h3>
+              <div className='grid grid-cols-2 gap-2'>
+                <Image src="image/gallery/img1.avif" alt="" width={800} height={800} className='w-[100px] h-[100px] object-cover '/>
+                <Image src="image/gallery/img2.avif" alt="" width={800} height={800} className='w-[100px] h-[100px] object-cover '/>
+                <Image src="image/gallery/img3.webp" alt="" width={800} height={800} className='w-[100px] h-[100px] object-cover '/>
+                <Image src="image/gallery/img4.webp" alt="" width={800} height={800} className='w-[100px] h-[100px] object-cover '/>
+              </div>
             </div>
           </div>
-
-          <div className='flex justify-center gap-3 fill-white'>
-            <Link href="https://www.tiktok.com/@balimmo?_t=8losSwiYXXW&_r=1" aria-label='socialmedia' target='_blank' className='fill-white flex items-center justify-center text-white bg-white/20 text-center w-[40px] h-[40px] rounded-full hover:bg-secondary hover:-translate-y-2 duration-500 transition-all'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M19.59 6.69a4.83 4.83 0 0 1-3.77-4.25V2h-3.45v13.67a2.89 2.89 0 0 1-5.2 1.74 2.89 2.89 0 0 1 2.31-4.64 2.93 2.93 0 0 1 .88.13V9.4a6.84 6.84 0 0 0-1-.05A6.33 6.33 0 0 0 5 20.1a6.34 6.34 0 0 0 10.86-4.43v-7a8.16 8.16 0 0 0 4.77 1.52v-3.4a4.85 4.85 0 0 1-1-.1z"></path></svg>
-            </Link>
-            <Link href="https://www.instagram.com/balimmo_villa?igsh=MXJhY2xkMXBnaGplbA==" aria-label='socialmedia' target='_blank' className='fill-white flex items-center justify-center text-white bg-white/20 text-center w-[40px] h-[40px] rounded-full hover:bg-secondary hover:-translate-y-2 duration-500 transition-all'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M11.999 7.377a4.623 4.623 0 1 0 0 9.248 4.623 4.623 0 0 0 0-9.248zm0 7.627a3.004 3.004 0 1 1 0-6.008 3.004 3.004 0 0 1 0 6.008z"></path><circle cx="16.806" cy="7.207" r="1.078"></circle><path d="M20.533 6.111A4.605 4.605 0 0 0 17.9 3.479a6.606 6.606 0 0 0-2.186-.42c-.963-.042-1.268-.054-3.71-.054s-2.755 0-3.71.054a6.554 6.554 0 0 0-2.184.42 4.6 4.6 0 0 0-2.633 2.632 6.585 6.585 0 0 0-.419 2.186c-.043.962-.056 1.267-.056 3.71 0 2.442 0 2.753.056 3.71.015.748.156 1.486.419 2.187a4.61 4.61 0 0 0 2.634 2.632 6.584 6.584 0 0 0 2.185.45c.963.042 1.268.055 3.71.055s2.755 0 3.71-.055a6.615 6.615 0 0 0 2.186-.419 4.613 4.613 0 0 0 2.633-2.633c.263-.7.404-1.438.419-2.186.043-.962.056-1.267.056-3.71s0-2.753-.056-3.71a6.581 6.581 0 0 0-.421-2.217zm-1.218 9.532a5.043 5.043 0 0 1-.311 1.688 2.987 2.987 0 0 1-1.712 1.711 4.985 4.985 0 0 1-1.67.311c-.95.044-1.218.055-3.654.055-2.438 0-2.687 0-3.655-.055a4.96 4.96 0 0 1-1.669-.311 2.985 2.985 0 0 1-1.719-1.711 5.08 5.08 0 0 1-.311-1.669c-.043-.95-.053-1.218-.053-3.654 0-2.437 0-2.686.053-3.655a5.038 5.038 0 0 1 .311-1.687c.305-.789.93-1.41 1.719-1.712a5.01 5.01 0 0 1 1.669-.311c.951-.043 1.218-.055 3.655-.055s2.687 0 3.654.055a4.96 4.96 0 0 1 1.67.311 2.991 2.991 0 0 1 1.712 1.712 5.08 5.08 0 0 1 .311 1.669c.043.951.054 1.218.054 3.655 0 2.436 0 2.698-.043 3.654h-.011z"></path></svg>
-            </Link>
-            <Link href="https://www.facebook.com/people/Balimmo/61557243686168/" aria-label='socialmedia' target='_blank' className='fill-white flex items-center justify-center text-white bg-white/20 text-center w-[40px] h-[40px] rounded-full hover:bg-secondary hover:-translate-y-2 duration-500 transition-all'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.397 20.997v-8.196h2.765l.411-3.209h-3.176V7.548c0-.926.258-1.56 1.587-1.56h1.684V3.127A22.336 22.336 0 0 0 14.201 3c-2.444 0-4.122 1.492-4.122 4.231v2.355H7.332v3.209h2.753v8.202h3.312z"></path></svg>
-            </Link>
-            <Link href="http://www.balimmo.fr/" aria-label='socialmedia' target='_blank' className='fill-white flex items-center justify-center text-white bg-white/20 text-center w-[40px] h-[40px] rounded-full hover:bg-secondary hover:-translate-y-2 duration-500 transition-all'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 2C6.486 2 2 6.486 2 12s4.486 10 10 10 10-4.486 10-10S17.514 2 12 2zM4 12c0-.899.156-1.762.431-2.569L6 11l2 2v2l2 2 1 1v1.931C7.061 19.436 4 16.072 4 12zm14.33 4.873C17.677 16.347 16.687 16 16 16v-1a2 2 0 0 0-2-2h-4v-3a2 2 0 0 0 2-2V7h1a2 2 0 0 0 2-2v-.411C17.928 5.778 20 8.65 20 12a7.947 7.947 0 0 1-1.67 4.873z"></path></svg>
-            </Link>
-            <Link href="https://youtube.com/@Balimmo_villa?si=bKpDdraIwv4n1nWI" aria-label='socialmedia' target='_blank' className='fill-white flex items-center justify-center text-white bg-white/20 text-center w-[40px] h-[40px] rounded-full hover:bg-secondary hover:-translate-y-2 duration-500 transition-all'>
-              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M21.593 7.203a2.506 2.506 0 0 0-1.762-1.766C18.265 5.007 12 5 12 5s-6.264-.007-7.831.404a2.56 2.56 0 0 0-1.766 1.778c-.413 1.566-.417 4.814-.417 4.814s-.004 3.264.406 4.814c.23.857.905 1.534 1.763 1.765 1.582.43 7.83.437 7.83.437s6.265.007 7.831-.403a2.515 2.515 0 0 0 1.767-1.763c.414-1.565.417-4.812.417-4.812s.02-3.265-.407-4.831zM9.996 15.005l.005-6 5.207 3.005-5.212 2.995z"></path></svg>
-            </Link>
-          </div>          
         </div>
       </div>
-    </section>
-
-    <section className='bg-secondary text-white text-center p-4 border-t border-white/50 text-sm divide-x flex flex-wrap items-center justify-center gap-3'>
-      <Link href='https://balimmo.fr/' className='hover:text-primary' target='_blank' aria-label='informations'>
-        PT BALIMMO DEVELOPMENT GROUP © 2021 - 2025
-      </Link>
-      <Link href='mentions-legales' className='pl-3 hover:text-primary' aria-label='informations'>
-        Mentions légales
-      </Link>
-      <Link href='cgv' className='pl-3 hover:text-primary' aria-label='informations'>
-        CGV
-      </Link>
-      <Link href='politique-de-confidentialite' className='pl-3 hover:text-primary' aria-label='informations'>
-        Politique de confidentialité
-      </Link>
-    </section>
     </>
   )
 }
